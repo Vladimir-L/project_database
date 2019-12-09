ceda0f298f6a9fadab94d72b10e3947099ae1254
Volodymyr Lopatkin <lopatkin.vladimir88@gmail.com>
2019-12-09 23:24:38 +0200

    error correction in queries

--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
-# quuery #1
+# query #1
 SELECT * FROM employe
     ORDER BY employe_last_name;
 
-# quuery #2
+# query #2
 SELECT e.employe_first_name, e.employe_last_name,
     AVG(sl.monthly_salary) AS average_monthly_salary
 FROM employe AS e
     INNER JOIN salary_log AS sl ON e.employe_id=sl.employe_id
 GROUP BY sl.employe_id;
 
-# quuery #3
+# query #3
 SELECT p.position_name,
     AVG(sl.monthly_salary) AS average_monthly_salary,
     SUM(sl.monthly_salary) AS total_monthly_salary
@@ -18,7 +18,7 @@ FROM position AS p
     LEFT JOIN salary_log AS sl ON e.employe_id=sl.employe_id
 GROUP BY p.position_id;
 
-# quuery #4
+# query #4
 SELECT e.employe_first_name, e.employe_last_name,
     COUNT(dtl.employe_id) AS total_working_day,
     SUM(dtl.income) AS total_income
@@ -26,7 +26,7 @@ FROM daily_transport_log AS dtl
     INNER JOIN employe AS e ON e.employe_id=dtl.employe_id
 GROUP BY dtl.employe_id;
 
-# quuery #5
+# query #5
 SELECT t.transport_name,
        COUNT(dtl.employe_id) AS total_working_day,
        AVG(dtl.income) AS avg_income,
@@ -36,12 +36,12 @@ FROM daily_transport_log AS dtl
 GROUP BY dtl.transport_id
 ORDER BY total_income DESC;
 
-# quuery #6
+# query #6
 SELECT employe_first_name, employe_last_name, dob
 FROM employe
 WHERE MONTH(dob) = 5;
 
-# quuery #7
+# query #7
 SELECT employe_first_name, employe_last_name,
     TIMESTAMPDIFF(YEAR, employment_date, CURDATE()) AS work_experience
 FROM employe;
